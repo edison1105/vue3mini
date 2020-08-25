f4ffc02b89b30b2dae997caf4b3494b3436a1f3a
daiwei <daiwei521@126.com>
2020-08-25 23:15:39 +0800

    render function

--- src/vue3-mini.ts
+++ src/vue3-mini.ts
@@ -1,3 +1,11 @@
-export function h(vnode:any, container:any) {
-  console.log('hhh')
+export function h(vnode, container) {
+  mountElement(vnode,container)
+}
+
+function mountElement(vnode, container) {
+  const { type, props, children } = vnode
+
+  const el = document.createElement(type)
+  el.textContent = children
+  container.appendChild(el)
 }
