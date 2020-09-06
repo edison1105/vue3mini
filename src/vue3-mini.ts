f9e755c9731934f044abd511a40767304435e009
daiwei <daiwei521@126.com>
2020-09-06 19:58:18 +0800

    add get sequece

--- src/vue3-mini.ts
+++ src/vue3-mini.ts
@@ -112,12 +112,20 @@ function patchKeyedChildren(c1, c2, container) {
     }
   }
 
+  // 忽略掉新增的
+  const lst = getSequence(newIndexToOldIndexMap.filter(d => d >= 0))
+  let j = lst.length - 1
   for (i = e2; i >= 0; i--) {
     const newChild = c2[i]
 
     //新增
     if (newIndexToOldIndexMap[i] === -1) {
       patch(null, newChild, container)
+    }
+
+    if (i == lst[j]) {
+      j--
+      continue
     }
 
     // 移动
@@ -184,4 +192,47 @@ function mountChildren(children, container) {
   children.forEach(child => {
     patch(null, child, container)
   })
+}
+
+
+function getSequence(arr) {
+  const p = arr.slice()                 //  保存原始数据
+  const result = [0]                    //  存储最长增长子序列的索引数组
+  let i, j, u, v, c
+  const len = arr.length
+  for (i = 0; i < len; i++) {
+    const arrI = arr[i]
+    if (arrI !== 0) {
+      j = result[result.length - 1]     //  j是子序列索引最后一项
+      if (arr[j] < arrI) {              //  如果arr[i] > arr[j], 当前值比最后一项还大，可以直接push到索引数组(result)中去
+        p[i] = j                        //  p记录第i个位置的索引变为j
+        result.push(i)
+        continue
+      }
+      u = 0                             //  数组的第一项
+      v = result.length - 1             //  数组的最后一项
+      while (u < v) {                   //  如果arrI <= arr[j] 通过二分查找，将i插入到result对应位置；u和v相等时循环停止
+        c = ((u + v) / 2) | 0           //  二分查找
+        if (arr[result[c]] < arrI) {
+          u = c + 1                     //  移动u
+        } else {
+          v = c                         //  中间的位置大于等于i,v=c
+        }
+      }
+      if (arrI < arr[result[u]]) {
+        if (u > 0) {
+          p[i] = result[u - 1]          //  记录修改的索引
+        }
+        result[u] = i                   //  更新索引数组(result)
+      }
+    }
+  }
+  u = result.length
+  v = result[u - 1]
+  //把u值赋给result
+  while (u-- > 0) {                     //  最后通过p数组对result数组进行进行修订，取得正确的索引
+    result[u] = v
+    v = p[v];
+  }
+  return result
 }
